ccd4f74af5b209c37221264a6e0ac4809aa479fa
Akos Morvai <akos.morvai@gpstuner.com>
2023-04-28 16:17:00 +0200

    Initial Commit

---
+++ GPXRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPXRecorder
+//
+//  Created by Ákos Morvai on 2023. 04. 28..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
